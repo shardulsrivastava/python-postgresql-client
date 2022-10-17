ada9c33f9ee5551044cc7b4ca582c3cc0f69d13a
Shardul <shradul.srivastava@hellofresh.com>
2022-10-18 01:27:29 +0200

    Add logging

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import psycopg2
 import os
 import time
+import logging
+import sys
+
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', stream=sys.stdout, level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S')
 
 DATABASE_URL = os.environ.get("DATABASE_URL", "localhost")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "tgam")
 DATABASE_USERNAME = os.environ.get("DATABASE_USERNAME", "postgres")
 DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD", "postgres")
-DATABASE_PORT = os.environ.get("DATABASE_PORT", "5432")
-
 DATABASE_PORT = os.environ.get("DATABASE_PORT", "5432")
 
 SLEEP_TIME = os.environ.get("SLEEP_TIME", "0.1")
@@ -22,7 +24,7 @@ def connect():
             port=DATABASE_PORT
         )
     except psycopg2.OperationalError:
-        print("Database is not reachable")
+        logging.error("Database is not reachable")
 
 
 def execute_ping(connection):
@@ -31,15 +33,16 @@ def execute_ping(connection):
     try:
         cur = connection.cursor()
         cur.execute('SELECT 1')
-        print("Database is reachable.")
+        logging.info("Database is reachable.")
     except psycopg2.OperationalError:
         pass
     #print(connection.closed)  # 2
 
 
 def ping_postgres():
-    print("Connecting to the database =>", DATABASE_URL)
+    logging.info(f"Connecting to the database =>{DATABASE_URL}")
     connection = connect()
+    logging.info(f"Connection established => {connection}")
     while True:
         execute_ping(connection)
         # Sleep for 50 miliseconds by default
@@ -47,4 +50,5 @@ def ping_postgres():
 
 
 if __name__ == '__main__':
+    logging.info("Start Python Postgres Client")
     ping_postgres()
